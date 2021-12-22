9a9bf71350b6c8250fdde757de067ffbd3e9f870
Bartosz Panic <bartosz.panic@gmail.com>
2021-12-22 18:32:27 +0100

    Add project files.

---
+++ RuchyBrowna/Point.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RuchyBrowna
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public double[] arrayX;
+        public double[] arrayY;
+
+        public void CalculateMoves(int n)
+        {
+            var random = new Random();
+            double fi, s;
+            arrayX = new double[n];
+            arrayY = new double[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                fi = Math.PI * random.Next() / Int32.MaxValue * 2;
+                Y = Y + Math.Sin(fi);
+                arrayY[i] = Y;
+                X = X + Math.Cos(fi);
+                arrayX[i] = X;
+            }
+            s = Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+            Console.WriteLine("Czasteczka przemiescila sie na odleglosc: {0}", s);
+        }
+
+    }
+}
---
+++ RuchyBrowna/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace RuchyBrowna
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Podaj ilość prób");
+            int n = int.Parse(Console.ReadLine());
+            Point point = new Point();
+            point.CalculateMoves(n);
+            SaveToXls(point.arrayX,point.arrayY);
+            Console.ReadKey();
+
+        }
+        private static void SaveToXls(double[] x, double[] y)
+        {
+            string path = Directory.GetCurrentDirectory();
+            using (StreamWriter sw = new StreamWriter("RuchyBrowna.xls"))
+            {
+                if (x.Length == y.Length)
+                {
+                    for (int i = 0; i < x.Length; i++)
+                    {
+                        sw.WriteLine(x[i] + "\t" + y[i]);
+                    }
+                }
+            }
+            Console.WriteLine("Plik znajduje się w {0}", path);
+        }
+    }
+
+}
